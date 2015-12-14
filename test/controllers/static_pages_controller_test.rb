aaf3508197ace354c483ba9c35952e925177b7d5
Andry Prakoso <rajakebo@gmail.com>
2015-12-14 06:52:30 +0000

    Home and Signup page created

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Be Active!"
   end
 
   test "should get signup" do
     get :signup
     assert_response :success
+    assert_select "title", "Sign Up | Be Active!"
   end
 
 end
