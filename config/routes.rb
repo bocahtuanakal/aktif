5d1610d1eda74e67af6db0aa1f8246d84ad1e3c8
Andry Prakoso <rajakebo@gmail.com>
2015-12-18 06:30:54 +0000

    Finish layout and user new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
 
-  get 'static_pages/signup'
+
+  root 'static_pages#home'
+  get 'aboutus'   => 'static_pages#aboutus'
+  get 'signup'  => 'users#new'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Be Active!"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Be Active!"
+    assert_select "title", "#{@base_title}"
   end
 
-  test "should get signup" do
-    get :signup
+  test "should get aboutus" do
+    get :aboutus
+
     assert_response :success
-    assert_select "title", "Sign Up | Be Active!"
+    assert_select "title", "About Us | #{@base_title}"
   end
 
 end
