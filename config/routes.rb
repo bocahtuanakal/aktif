919214a2eb6b237b5b05ea97dfe05f6586c03787
Andry Prakoso <rajakebo@gmail.com>
2016-01-03 13:09:37 +0000

    finish login logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
-
   root 'static_pages#home'
   get 'aboutus'   => 'static_pages#aboutus'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   resources :users
 
   
